9f00c6bc7d4dcb5458275b48329368a2f1d31ad3
irenkamalova <iren.kamalova@gmail.com>
2015-11-20 21:45:13 +0400

    New pages were created and header for pages in b.js was done

---
+++ b.js
@@ -0,0 +1,42 @@
+document.write('\
+\
+\
+		<div id="includedContent"></div>\
+   	<div class="container" style="margin-top: 0px">\
+			<div class="row">\
+				<div class="col-md-12">\
+					<h1 class="text-style" \
+style="background-image: url(file:///C:/Users/Иришка/Documents/GitHub/DgayantiBlog/background.jpg)\
+\
+" > \
+TEXT</h1> \
+</div>\
+</div>\
+\
+			<div class="row">\
+			<nav class="navbar navbar-default ">\
+			<div class="navbar-collapse collapse">\
+			<ul class="nav navbar-nav">\
+					<li class="dropdown">\
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">\
+							О нас<b class="caret"></b></a>\
+						<ul class="dropdown-menu">\
+						<li><a href="services.html">История студии</a></li>\
+						<li class="divider"></li>\
+						<li><a href="schools.html">Наши направления</a></li></ul>\
+					</li>\
+					<li><a href="preps.html">Наши преподаватели</a></li>\
+					<li><a href="photogallery.html">Фото галерея</a></li>\
+					<li><a href="videogallery.html">Видео галерея</a></li>\
+					</ul>\
+					 <div class="navbar-header navbar-right">\
+				  		<p class="navbar-text">\
+				  		<a href="#" class="navbar-link">Вход</a> //todo - module\
+				  		</p>\
+				  	</div>\
+				</div>\
+			</div>\
+			</nav>\
+\
+\
+');
