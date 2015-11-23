02d9c2702a7d60b6e0c9c81ffd73b221b0656fd8
Irina <iren.kamalova@gmail.com>
2015-11-24 01:41:05 +0300

    Experiments with header

--- b.js
+++ b.js
@@ -1,22 +1,14 @@
 document.write('\
 \
 \
-		<div id="includedContent"></div>\
+<div id="includedContent"></div>\
    	<div class="container" style="margin-top: 0px">\
-			<div class="row">\
-				<div class="col-md-12">\
-					<h1 class="text-style" \
-style="background-image: url(file:///C:/Users/Иришка/Documents/GitHub/DgayantiBlog/background.jpg)\
 \
-" > \
-TEXT</h1> \
-</div>\
-</div>\
 \
 			<div class="row">\
 			<nav class="navbar navbar-default ">\
-			<div class="navbar-collapse collapse">\
-			<ul class="nav navbar-nav">\
+			<div class="navbar-collapse collapse ">\
+			<ul class="nav navbar-nav ">\
 					<li class="dropdown">\
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown">\
 							О нас<b class="caret"></b></a>\
@@ -29,10 +21,10 @@ TEXT</h1> \
 					<li><a href="photogallery.html">Фото галерея</a></li>\
 					<li><a href="videogallery.html">Видео галерея</a></li>\
 					</ul>\
-					 <div class="navbar-header navbar-right">\
-				  		<p class="navbar-text">\
-				  		<a href="#" class="navbar-link">Вход</a> //todo - module\
-				  		</p>\
+					 <div class="navbar-default navbar-right">\
+				  		\
+				  		<a href="#" class="navbar-text">Вход</a>\
+				  	\
 				  	</div>\
 				</div>\
 			</div>\
