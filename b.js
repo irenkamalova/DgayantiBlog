3da36cc0c6bacf100e24e6f8976df17a32d397a7
Irina <iren.kamalova@gmail.com>
2015-11-25 00:12:02 +0300

    Added pop-up, need to fix

--- b.js
+++ b.js
@@ -23,12 +23,32 @@ document.write('\
 					</ul>\
 					 <div class="navbar-default navbar-right">\
 				  		\
-				  		<a href="#" class="navbar-text">Вход</a>\
+				  		<a href="#" class="navbar-text" data-toggle="modal" data-target="#myModal">Вход</a>\
 				  	\
 				  	</div>\
 				</div>\
 			</div>\
 			</nav>\
+\
+\<!-- Modal -->\
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">\
+  <div class="modal-dialog" role="document">\
+    <div class="modal-content">\
+      <div class="modal-header">\
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>\
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>\
+      </div>\
+      <div class="modal-body">\
+        <img src="./bootstrap-prestructure_files/555x300">\
+        PamPamPam\
+      </div>\
+      <div class="modal-footer">\
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
+        <button type="button" class="btn btn-primary">Save changes</button>\
+      </div>\
+    </div>\
+  </div>\
+</div>\
 \
 \
 ');
